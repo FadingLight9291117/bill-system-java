98ae53ad9b75b9464d04dfc828a250c7d04baa2c
clz <fadinglight9291117@126.com>
2021-08-11 00:38:15 +0800

    init

---
+++ src/test/java/com/fadinglight/billsystem/BillSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadinglight.billsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
